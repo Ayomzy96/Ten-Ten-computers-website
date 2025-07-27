329637336e37d832a158739a351b1e344a3329da
Ayomzy96 <abrahamayomide117@gmail.com>
2025-07-27 08:05:56 +0100

    vite config altered

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { resolve } from 'path'
 export default {
   root: resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true,
   },
   server: {
     port: 8080
